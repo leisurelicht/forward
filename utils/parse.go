62d6176decda84f2c3dc31d5cfed50ae8ac8dd9a
暮晨 <leisurelicht@gmail.com>
2020-11-25 20:36:44 +0800

    修改变量名

--- utils/parse.go
+++ utils/parse.go
@@ -1,30 +1,30 @@
 package utils
 
 import (
-    "os"
-    "strings"
-    "github.com/leisurelicht/forward/Protocol"
+	"github.com/leisurelicht/forward/Protocol"
+	"os"
+	"strings"
 
-    kp "gopkg.in/alecthomas/kingpin.v2"
+	kp "gopkg.in/alecthomas/kingpin.v2"
 )
 
 const (
-	Version  = "0.0.1"
+	Version = "0.0.1"
 )
 
 func ParaseArgs() *Protocol.TCPArgs {
-	args := &Protocol.TCPArgs{}
+	tcpArgs := &Protocol.TCPArgs{}
 
 	app := kp.New("forward", "A Simple Traffic Forwarding Tools.")
 	app.Author("MuCheng").Version(Version)
 
 	tcp := app.Command("tcp", "TCP Traffic Forward")
-	args.ListenIP = tcp.Flag("listen ip", "the ip is under watch").Short('l').Default("").String()
-	args.ListenPort = tcp.Flag("listen port", "the port is under watch").Short('p').Required().Int()
-	args.ForwardIP = tcp.Flag("forward ip", "forward target ip").Short('F').Required().String()
-	args.ForwardPort = tcp.Flag("forward port", "forward target port").Short('P').Required().Int()
+	tcpArgs.ListenIP = tcp.Flag("listen ip", "the ip is under watch").Short('l').Default("").String()
+	tcpArgs.ListenPort = tcp.Flag("listen port", "the port is under watch").Short('p').Required().Int()
+	tcpArgs.ForwardIP = tcp.Flag("forward ip", "forward target ip").Short('F').Required().String()
+	tcpArgs.ForwardPort = tcp.Flag("forward port", "forward target port").Short('P').Required().Int()
 
-	args.Protocol = strings.ToLower(kp.MustParse(app.Parse(os.Args[1:])))
+	tcpArgs.Protocol = strings.ToLower(kp.MustParse(app.Parse(os.Args[1:])))
 
-	return args
+	return tcpArgs
 }
